7e63692ee970bad982c51669a93e3fe3a86c4a8d
Gustavo Ledesma <gustavo.lede@gmail.com>
2024-05-15 00:15:30 -0300

    varias modiifcaciones

--- js/script-contacto.js
+++ js/script-contacto.js
@@ -1,11 +1,12 @@
 function validateForm() {
     var nombre = document.getElementById('nombre').value;
     var email = document.getElementById('email').value;
+    var celular = document.getElementById('celular').value;
     var mensaje = document.getElementById('mensaje').value;
     var motivo = document.getElementById('motivo').value;
     var contactMethod = document.querySelector('input[name="contactMethod"]:checked');
 
-    if (nombre === '' || email === '' || mensaje === '' || motivo === '' || contactMethod === null) {
+    if (nombre === '' || email === ''|| celular === '' || mensaje === '' || motivo === '' || contactMethod === null) {
         alert('Por favor, completa todos los campos obligatorios.');
         return false;
     }
